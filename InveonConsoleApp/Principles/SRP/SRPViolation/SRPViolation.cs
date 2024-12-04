268ea371672bb0f4da1b7cb4d5e3c5a3133f0162
kemalege <cirpanliege@gmail.com>
2024-12-04 21:28:04 +0300

    SRP violation and applied Mediator to resolve SRP violation example

---
+++ InveonConsoleApp/Principles/SRP/SRPViolation/SRPViolation.cs
@@ -0,0 +1,15 @@
+﻿namespace InveonConsoleApp.Principles.SRP.SRPViolation;
+
+public class UserManager
+{
+    public void CreateUser(string username)
+    {
+        Console.WriteLine($"User {username} created.");
+        SendEmail(username);
+    }
+
+    private void SendEmail(string username)
+    {
+        Console.WriteLine($"Email sent to {username}");
+    }
+}
