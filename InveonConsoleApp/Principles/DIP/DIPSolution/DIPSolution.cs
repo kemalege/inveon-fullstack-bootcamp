bd96375ae353f9df88aa3aa89f19f2ea837da11f
kemalege <cirpanliege@gmail.com>
2024-12-04 22:41:24 +0300

    DIP violation and resolve DIP violation using Dependency Injection example

---
+++ InveonConsoleApp/Principles/DIP/DIPSolution/DIPSolution.cs
@@ -0,0 +1,37 @@
+﻿namespace InveonConsoleApp.Principles.DIP.DIPSolution;
+
+public interface IMessageSender
+{
+    void SendMessage(string message);
+}
+
+public class EmailSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"Email sent: {message}");
+    }
+}
+
+public class SmsSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"SMS sent: {message}");
+    }
+}
+
+public class Notification
+{
+    private readonly IMessageSender _messageSender;
+
+    public Notification(IMessageSender messageSender)
+    {
+        _messageSender = messageSender;
+    }
+
+    public void Send(string message)
+    {
+        _messageSender.SendMessage(message);
+    }
+}
--- InveonConsoleApp/Program.cs
+++ InveonConsoleApp/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using InveonConsoleApp.Principles.DIP.DIPSolution;
 using InveonConsoleApp.Principles.ISP.ISPSolution;
 using InveonConsoleApp.Principles.LSP.LSPSolution;
 using InveonConsoleApp.Principles.OCP.OCPSolution;
 using InveonConsoleApp.Principles.SRP.WithSRP;
+
+using ViolationNotification = InveonConsoleApp.Principles.DIP.DIPViolation.Notification;
+using DipNotification = InveonConsoleApp.Principles.DIP.DIPSolution.Notification;
+
 
 namespace InveonBootcamp
 {
@@ -38,6 +43,15 @@ namespace InveonBootcamp
 
             IDeliveryActions deliveryPerson = new DeliveryPerson();
             deliveryPerson.DeliverOrder();
+            
+            var notification = new ViolationNotification();
+            notification.Send("Hello, DIP violation!");
+            
+            var emailNotification = new DipNotification(new EmailSender());
+            emailNotification.Send("Hello via Email!");
+
+            var smsNotification = new DipNotification(new SmsSender());
+            smsNotification.Send("Hello via SMS!");
 
 
         }
