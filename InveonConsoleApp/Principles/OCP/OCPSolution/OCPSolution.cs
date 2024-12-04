1275f01c169e84f60dc14d209aa79c27ae3b3030
kemalege <cirpanliege@gmail.com>
2024-12-04 21:29:31 +0300

    OCP violation OCP Solution example

---
+++ InveonConsoleApp/Principles/OCP/OCPSolution/OCPSolution.cs
@@ -0,0 +1,32 @@
+﻿namespace InveonConsoleApp.Principles.OCP.OCPSolution;
+
+public interface IPaymentMethod
+{
+    void ProcessPayment();
+}
+
+public class CreditCard : IPaymentMethod
+{
+    public void ProcessPayment()
+    {
+        Console.WriteLine("Processing credit card payment...");
+    }
+}
+
+public class PayPal : IPaymentMethod
+{
+    public void ProcessPayment()
+    {
+        Console.WriteLine("Processing PayPal payment...");
+    }
+}
+
+public class PaymentProcessor(IPaymentMethod paymentMethod)
+{
+    private readonly IPaymentMethod _paymentMethod = paymentMethod;
+
+    public void Process()
+    {
+        _paymentMethod.ProcessPayment();
+    }
+}
---
+++ InveonConsoleApp/Principles/OCP/OCPViolation/OCPViolation.cs
@@ -0,0 +1,20 @@
+﻿namespace InveonConsoleApp.Principles.OCP.OCPViolation;
+
+public class PaymentProcessor
+{
+    public void ProcessPayment(string paymentMethod)
+    {
+        if (paymentMethod == "CreditCard")
+        {
+            Console.WriteLine("Processing credit card payment...");
+        }
+        else if (paymentMethod == "PayPal")
+        {
+            Console.WriteLine("Processing PayPal payment...");
+        }
+        else
+        {
+            throw new Exception("Unsupported payment method.");
+        }
+    }
+}
