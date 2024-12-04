41bc72381017ae6bad1d36e256cc3d5448b8818e
kemalege <cirpanliege@gmail.com>
2024-12-04 21:29:43 +0300

    LSP violation LSP Solution example

---
+++ InveonConsoleApp/Principles/LSP/LSPViolation/LSPViolation.cs
@@ -0,0 +1,33 @@
+﻿namespace InveonConsoleApp.Principles.LSP.LSPViolation;
+
+public interface IVehicle
+{
+    void Start();
+    void Move();
+}
+
+public class Car : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Car engine started.");
+    }
+
+    public void Move()
+    {
+        Console.WriteLine("Car is moving.");
+    }
+}
+
+public class Bicycle : IVehicle
+{
+    public void Start()
+    {
+        throw new NotImplementedException("Bicycle cannot start an engine.");
+    }
+
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is pedaled forward.");
+    }
+}
--- InveonConsoleApp/Program.cs
+++ InveonConsoleApp/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using InveonConsoleApp.Principles.LSP.LSPSolution;
+using InveonConsoleApp.Principles.OCP.OCPSolution;
+using InveonConsoleApp.Principles.SRP.WithSRP;
 
-Console.WriteLine("Hello, World!");
+namespace InveonBootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notificationService = new NotificationService();
+            var mediator = new UserMediator(notificationService);
+
+            var userManager = new UserManager(mediator);
+            userManager.CreateUser("kemal_ege");
+            
+            // var processor = new PaymentProcessor();
+            // processor.ProcessPayment("CreditCard");
+            // processor.ProcessPayment("PayPal");
+            
+            var paypalPayment = new PayPal();
+            var paypalProcessor = new PaymentProcessor(paypalPayment);
+            paypalProcessor.Process();
+            
+            IMotorVehicle car = new Car();
+            car.StartEngine();
+            car.Move();
+
+            INonMotorVehicle bicycle = new Bicycle();
+            bicycle.StartPedaling();
+            bicycle.Move();
+
+        }
+    }
+}
