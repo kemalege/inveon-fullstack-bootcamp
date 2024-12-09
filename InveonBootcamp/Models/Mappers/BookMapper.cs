668394df0324833d36e3b8b67bc00811e4c73b05
kemalege <cirpanliege@gmail.com>
2024-12-10 00:25:59 +0300

    Create BookMapper

---
+++ InveonBootcamp/Models/Mappers/BookMapper.cs
@@ -0,0 +1,18 @@
+﻿using InveonBootcamp.Models.Repositories;
+
+namespace InveonBootcamp.Models.Mappers;
+
+public static class BookMapper
+{
+    public static BookDto ToDto(this Book book)
+    {
+        return new BookDto
+        {
+            Id = book.Id,
+            Title = book.Title,
+            Author = book.Author,
+            Description = book.Description,
+            Price = book.Price
+        };
+    }
+}
